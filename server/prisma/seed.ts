edcb6154f144d028c535cd2bb54abb8ca41c864d
Gubanov Igor <igubanov@cs-cart.com>
2022-02-12 14:46:37 +0300

    Made seeder for sources.

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -15,6 +15,8 @@ async function main() {
     },
   })
 
+  seedSources()
+
   // TODO: Insert some sources here.
 
   console.log({ user })
@@ -27,3 +29,24 @@ main()
   .finally(async () => {
     await prisma.$disconnect()
   })
+
+const sourcesUrl = [
+  'https://news.un.org/feed/subscribe/ru/tags/piracy/feed/rss.xml',
+  'https://news.un.org/feed/subscribe/ru/tags/disarmament/feed/rss.xml',
+  'https://news.un.org/feed/subscribe/ru/news/topic/human-rights/feed/rss.xml',
+  'https://news.un.org/ru/tags/disabled/feed/rss.xml',
+  'https://news.un.org/feed/subscribe/ru/tags/palestine/feed/rss.xml',
+  'https://news.un.org/feed/subscribe/ru/tags/dprk/feed/rss.xml',
+]
+
+const seedSources = () => {
+  sourcesUrl.forEach(async (sourceUrl) => {
+    await prisma.article.create({
+      data: {
+        name: '',
+        rssFeedUrl: sourceUrl,
+        feeds: [],
+      },
+    })
+  })
+}
